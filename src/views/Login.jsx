b31ff1907a5075a10a888aee775535d0a113ab70
BrayanMontenegro <montenegrobrayab45@gmail.com>
2025-03-14 20:08:10 -0600

    grupal3

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import LoginForm from "../Components/LoginForm";
+import LoginForm from "../components/LoginForm";
 import { appfirebase } from "../database/firebaseconfig";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useAuth } from "../database/authcontext";
