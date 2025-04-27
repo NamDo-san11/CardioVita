867d8c04a9dd506e61c74594d4bd6d6223db8671
Roxana Mendoza <mendozasanzcrisro@gmail.com>
2025-04-27 15:35:23 -0600

    Educacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,50 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Login from './views/Login'
+import Login from './views/Login';
 import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
 import SintomasView from "./views/SintomasView";
 import HeartRateMonitor from "./views/HeartRateMonitor";
 import PresionArterialView from "./views/PresionArterialView";
 import AlertasMedicacion from "./views/AlertasMedicacion";
+import EducacionView from "./views/EducacionView";  // <-- NUEVA IMPORTACIÓN
+
 import ReactGA from "react-ga4";
-
-import './App.css'
+import './App.css';
 import { useEffect } from "react";
-
-
-
 
 function App() {
 
   useEffect(() => {
-    // ?Iniciar Analityc la app
     ReactGA.initialize("G-ZPQ0YG91K6");
-  
     ReactGA.send({
       hitType: 'pageview',
       page: window.location.pathname,
       title: 'App.jsx'
-    })
+    });
   }, []);
-  
+
   return (
     <>
       <AuthProvider>
         <Router>
-            <Encabezado />
-            <main>
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
-                <Route path="/sintomas" element={<ProtectedRoute element={<SintomasView />} />} />
-                <Route path="/corazon" element={<ProtectedRoute element={<HeartRateMonitor />} />} />
-                <Route path="/presion" element={<ProtectedRoute element={<PresionArterialView />} />} />
-                <Route path="/alertasmedicacion" element={<ProtectedRoute element={<AlertasMedicacion />} />} />
-              </Routes>
-            </main>
+          <Encabezado />
+          <main>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+              <Route path="/sintomas" element={<ProtectedRoute element={<SintomasView />} />} />
+              <Route path="/corazon" element={<ProtectedRoute element={<HeartRateMonitor />} />} />
+              <Route path="/presion" element={<ProtectedRoute element={<PresionArterialView />} />} />
+              <Route path="/alertasmedicacion" element={<ProtectedRoute element={<AlertasMedicacion />} />} />
+              <Route path="/educacion" element={<ProtectedRoute element={<EducacionView />} />} /> {/* <-- NUEVA RUTA */}
+            </Routes>
+          </main>
         </Router>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
